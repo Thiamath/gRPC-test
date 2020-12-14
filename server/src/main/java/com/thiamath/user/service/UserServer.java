f10a81fcaaa7df6686880ea043ea933c2ad7b1ab
Thiamath <thiamath@gmail.com>
2020-12-14 01:58:28 +0100

    Cleaning up.

--- server/src/main/java/com/thiamath/user/service/UserServer.java
+++ server/src/main/java/com/thiamath/user/service/UserServer.java
@@ -11,7 +11,7 @@ public class UserServer {
     private final int port;
     private final Server server;
 
-    public UserServer(int port) throws IOException {
+    public UserServer(int port) {
         this(ServerBuilder.forPort(port), port);
     }
 
@@ -36,19 +36,16 @@ public class UserServer {
     public void start() throws IOException {
         server.start();
         System.out.println("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                try {
-                    UserServer.this.stop();
-                } catch (InterruptedException e) {
-                    e.printStackTrace(System.err);
-                }
-                System.err.println("*** server shut down");
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+            System.err.println("*** shutting down gRPC server since JVM is shutting down");
+            try {
+                UserServer.this.stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
             }
-        });
+            System.err.println("*** server shut down");
+        }));
     }
 
     /**
