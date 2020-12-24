4d6497cacd02b42fd8bb5a714b49999c044f365c
Thiamath <thiamath@gmail.com>
2020-12-24 12:24:35 +0100

    Put some logs to the server lines.

--- server/src/main/java/com/thiamath/user/service/UserService.java
+++ server/src/main/java/com/thiamath/user/service/UserService.java
@@ -4,10 +4,15 @@ import io.grpc.stub.StreamObserver;
 import test.thiamath.user.UserOuterClass;
 import test.thiamath.user.UserServiceGrpc.UserServiceImplBase;
 
+import java.util.logging.Logger;
+
 public class UserService extends UserServiceImplBase {
+    private static final Logger logger = Logger.getLogger(UserService.class.getName());
+
     @Override
     public void getUser(UserOuterClass.User request, StreamObserver<UserOuterClass.User> responseObserver) {
-        responseObserver.onNext(UserOuterClass.User.newBuilder().build());
+        logger.info("User requested " + request.getName());
+        responseObserver.onNext(request);
         responseObserver.onCompleted();
     }
 }
