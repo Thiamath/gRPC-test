1b288b5e0d41fd1bd1d53cc04f159c97a6795b5b
Thiamath <thiamath@gmail.com>
2021-03-09 21:37:50 +0100

    Adding some colored feedback to the server

--- server/src/main/java/com/thiamath/user/service/UserService.java
+++ server/src/main/java/com/thiamath/user/service/UserService.java
@@ -1,10 +1,15 @@
 package com.thiamath.user.service;
 
+import com.diogonunes.jcolor.Ansi;
+import com.diogonunes.jcolor.Attribute;
+import com.thiamath.user.UserOuterClass.ConversationStream;
 import com.thiamath.user.UserOuterClass.GetUserRequest;
+import com.thiamath.user.UserOuterClass.SendUserListResponse;
 import com.thiamath.user.UserOuterClass.User;
 import com.thiamath.user.UserServiceGrpc.UserServiceImplBase;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 public class UserService extends UserServiceImplBase {
@@ -12,8 +17,59 @@ public class UserService extends UserServiceImplBase {
 
     @Override
     public void getUser(GetUserRequest request, StreamObserver<User> responseObserver) {
-        logger.info("User requested " + request.getName());
+        System.out.println(Ansi.colorize("Received User request", Attribute.GREEN_TEXT()));
         responseObserver.onNext(User.newBuilder().setName(request.getName()).build());
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getUserList(GetUserRequest request, StreamObserver<User> responseObserver) {
+        System.out.println(Ansi.colorize("Received getUserList request", Attribute.BLUE_TEXT()));
+        for (int i = 0; i < 10; i++) {
+            responseObserver.onNext(User.newBuilder().setName("Listed user " + i).build());
+        }
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<User> sendUserList(StreamObserver<SendUserListResponse> responseObserver) {
+        final AtomicInteger count = new AtomicInteger(0);
+        return new StreamObserver<>() {
+            @Override
+            public void onNext(User value) {
+                count.incrementAndGet();
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(SendUserListResponse.newBuilder().setReceived(count.get()).build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    @Override
+    public StreamObserver<ConversationStream> conversation(StreamObserver<ConversationStream> responseObserver) {
+        return new StreamObserver<>() {
+            @Override
+            public void onNext(ConversationStream value) {
+                responseObserver.onNext(ConversationStream.newBuilder().setMessage("You said \"" + value.getMessage() + "\"").build());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
